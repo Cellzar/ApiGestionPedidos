53cdeba1722947328c2e64fc8c931241d2022e90
Cesar Torres Guerrero <c-sar-torres@hotmail.com>
2022-01-14 22:36:17 -0500

    Segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import {FormsModule} from '@angular/forms';
+import { CustomMaterialModule } from './custom-material/custom-material.module';
+import { BarraSuperiorComponent } from './components/barra-superior/barra-superior.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BarraSuperiorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    CustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
